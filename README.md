3d5f84850a6753e9c03bed1332624dad0c8cde8d
Daniel Andres <Dapinto7@misena.edu.co>
2016-04-06 00:35:10 -0500

    Cuenta lineas Para proyectos eleborado en java
    Esta aplicacion esta diseñada para leer
    archivos en texto plano codificado en java
    realiza el conteo de lienas, funciones y
    archivos creados en la aplicacion

---
+++ README.md
@@ -0,0 +1,7 @@
+Cuenta lineas
+Para proyectos eleborado en java
+
+Esta aplicacion esta diseñada para leer
+archivos en texto plano codificado en java
+realiza el conteo de lienas, funciones y
+archivos creados en la aplicacion 
---
+++ src/main.java
@@ -0,0 +1,20 @@
+import GUI.Interfaz;
+
+/*
+ * Nombre: Daniel Andres Pinto Castiblanco
+ * Fecha : 10/02/2016
+ * 
+ * Descripcion
+ * 
+ * La aplicacion realizara una lectura de codigo en texto para 
+ * saber la cantidad de lineas programadas, de metodos y de 
+ * clases sin contar la documentacion en el codigo y las buenas
+ * practicas 
+ */
+public class main {
+	public static void main(String[] args) {
+
+		new Interfaz();
+	}
+
+}
